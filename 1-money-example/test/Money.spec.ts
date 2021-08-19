78e9c53fb39aebb9eb8679d04f0c50c7d1684ff1
ninginx <publictaro@gmail.com>
2021-08-19 14:51:26 +0900

    solve side effect,amount

--- 1-money-example/test/Money.spec.ts
+++ 1-money-example/test/Money.spec.ts
@@ -4,7 +4,6 @@ describe('多国通貨対応', () => {
   //todo:
   // $5 + 10 CHF = 10$(レートが2:1の場合)
   // amountをprivateにする
-  // Dollarの副作用どうするか？
   // Moneyの丸め処理はどうするか？
 
   // it('$5 + 10 CHF = 10$(レートが2:1の場合)', () => {
@@ -12,7 +11,6 @@ describe('多国通貨対応', () => {
   //   expect(fiveDollar.times(2)).toEqual(10);
   // });
 
-  // Dollarの副作用どうするか？
   it('test multiplication', () => {
     const fiveDollar = new Dollar(5);
     let product = fiveDollar.times(2);

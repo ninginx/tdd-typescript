8dde71930ba9c2e941cdb492d0841f68c6640d05
ninginx <publictaro@gmail.com>
2021-08-21 06:49:50 +0900

    part 9 ongoin

--- 1-money-example/src/Money/money.ts
+++ 1-money-example/src/Money/money.ts
@@ -1,18 +1,33 @@
-class Money {
+export abstract class Money {
   protected amount: number;
-  constructor(amount: number) {
+  protected currency: string;
+  constructor(amount: number, currency: string) {
     this.amount = amount;
+    this.currency = currency;
   }
+
+  static dollar = (amount: number): Money => {
+    return new Dollar(amount);
+  };
+
+  static franc = (amount: number): Money => {
+    return new Franc(amount);
+  };
   equals = ({ amount, constructor }: Money): boolean => {
     return this.amount === amount && this.constructor.name === constructor.name;
+  };
+
+  abstract times(multiplier: number): Money;
+
+  myCurrency = (): string => {
+    return this.currency;
   };
 }
 
 export class Dollar extends Money {
   constructor(amount: number) {
-    super(amount);
+    super(amount, 'USD');
   }
-
   times = (multiplier: number): Dollar => {
     return new Dollar(multiplier * this.amount);
   };
@@ -20,10 +35,9 @@ export class Dollar extends Money {
 
 export class Franc extends Money {
   constructor(amount: number) {
-    super(amount);
+    super(amount, 'CHF');
   }
-
-  times = (multiplier: number): Franc => {
+  times = (multiplier: number): Dollar => {
     return new Franc(multiplier * this.amount);
   };
 }
--- 1-money-example/test/Money.spec.ts
+++ 1-money-example/test/Money.spec.ts
@@ -1,4 +1,4 @@
-import { Dollar, Franc } from '../src/Money';
+import { Dollar, Franc, Money } from '../src/Money';
 
 describe('多国通貨対応', () => {
   //todo:
@@ -22,34 +22,42 @@ describe('多国通貨対応', () => {
   // });
 
   it('test multiplication', () => {
-    const fiveDollar = new Dollar(5);
-    expect(fiveDollar.times(2).equals(new Dollar(10))).toBe(true);
-    expect(fiveDollar.times(3).equals(new Dollar(15))).toBe(true);
+    const fiveDollar: Money = Money.dollar(5);
+    expect(fiveDollar.times(2).equals(Money.dollar(10))).toBe(true);
+    expect(fiveDollar.times(3).equals(Money.dollar(15))).toBe(true);
   });
 
   it('test dollar equality true', () => {
-    expect(new Dollar(5).equals(new Dollar(5))).toBe(true);
+    expect(Money.dollar(5).equals(Money.dollar(5))).toBe(true);
   });
 
   it('test dollar equality false', () => {
-    expect(new Dollar(5).equals(new Dollar(6))).toBe(false);
+    expect(Money.dollar(5).equals(Money.dollar(6))).toBe(false);
   });
 
   it('test franc equality true', () => {
-    expect(new Franc(5).equals(new Franc(5))).toBe(true);
+    expect(Money.franc(5).equals(Money.franc(5))).toBe(true);
   });
 
   it('test franc equality false', () => {
-    expect(new Franc(5).equals(new Franc(6))).toBe(false);
+    expect(Money.franc(5).equals(Money.franc(6))).toBe(false);
   });
 
   it('test franc & dollar equality false', () => {
-    expect(new Franc(5).equals(new Dollar(5))).toBe(false);
+    expect(Money.franc(5).equals(Money.dollar(5))).toBe(false);
+  });
+
+  it('test dollar currency', () => {
+    expect(Money.dollar(5).myCurrency()).toEqual('USD');
+  });
+
+  it('test franc currency', () => {
+    expect(Money.franc(5).myCurrency()).toEqual('CHF');
   });
 
   it('test Fran Multiplication', () => {
-    const fiveFran = new Franc(5);
-    expect(fiveFran.times(2).equals(new Franc(10))).toBe(true);
-    expect(fiveFran.times(3).equals(new Franc(15))).toBe(true);
+    const fiveFran: Money = Money.franc(5);
+    expect(fiveFran.times(2).equals(Money.franc(10))).toBe(true);
+    expect(fiveFran.times(3).equals(Money.franc(15))).toBe(true);
   });
 });
